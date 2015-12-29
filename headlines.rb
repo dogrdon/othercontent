5b8cd4229c61da857a8817901f5f11cfb771b540
drew gordon <drew.s.gordon@gmail.com>
2015-12-28 19:05:09 -0500

    add more meta starters, headless scraping for ajax loaded stuff

--- headlines.rb
+++ headlines.rb
@@ -4,7 +4,25 @@
 getting the headlines that we'll use for inspiration
 =end
 
-require 'net/http'
+require 'nokogiri'
+require 'capybara/poltergeist'
 require 'mongo'
 require 'optparse'
 
+BROPTIONS = {:js_errors => false}
+TEST_BASE = 'http://www.nydailynews.com'
+
+
+Capybara.register_driver :poltergeist do |app|
+	Capybara::Poltergeist::Driver.new(app, BROPTIONS)
+end
+
+session = Capybara::Session.new(:poltergeist)
+
+session.visit TEST_BASE
+
+doc = Nokogiri::HTML(session.html)
+
+links = doc.css('div.teaser p a').map{ |l| l['href'] }[0..5]
+
+puts links
